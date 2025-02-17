30791867606ef44d6bd046a18421637fba5ee1af
Jay Sinnadurai <jay.sinnadurai@gmail.com>
2025-02-17 18:57:48 +0000

    Added notification css, shopping bag preview, and additional messages

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-dk558i%mkd%x(n3l5w2xj+!^^hqxatvc8-!b8ves(mum@%w2ej
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
