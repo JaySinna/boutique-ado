50d7e9e55b1a3e42343c43a9c332b7cec43678d5
Jay Sinnadurai <jay.sinnadurai@gmail.com>
2025-02-19 09:52:07 +0000

    Added basic checkout functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dk558i%mkd%x(n3l5w2xj+!^^hqxatvc8-!b8ves(mum@%w2ej'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -168,8 +171,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "django-insecure-dk558i%mkd%x(n3l5w2xj+!^^hqxatvc8-!b8ves(mum@%w2ej")
+os.environ.setdefault("STRIPE_PUBLIC_KEY", "pk_test_51Qtr7NPx2zbqPK2lE9PCKmnH1CRdrEGE3in6oNGoLRIeD6UlToozmfil07aa0gq1qHtaREjbpGZEKjKKGJQbgSQQ00UwDZbWtG")
+os.environ.setdefault("STRIPE_SECRET_KEY", "sk_test_51Qtr7NPx2zbqPK2lAZ1MZaeVkuIahLWGX6oBPq0LWe9EROuWSt8Ft8U6jNRKI0xCCxLMQE69OqOMk0iy4Y7O5vU100PN0bLWVq")
