dc7c125c494d36df0dd29b7b97b5b7a78c9be8fd
Jay Sinnadurai <jay.sinnadurai@gmail.com>
2025-02-25 12:49:38 +0000

    Tested webhooks

--- env.py
+++ env.py
@@ -3,3 +3,4 @@ import os
 os.environ.setdefault("SECRET_KEY", "django-insecure-dk558i%mkd%x(n3l5w2xj+!^^hqxatvc8-!b8ves(mum@%w2ej")
 os.environ.setdefault("STRIPE_PUBLIC_KEY", "pk_test_51Qtr7NPx2zbqPK2lE9PCKmnH1CRdrEGE3in6oNGoLRIeD6UlToozmfil07aa0gq1qHtaREjbpGZEKjKKGJQbgSQQ00UwDZbWtG")
 os.environ.setdefault("STRIPE_SECRET_KEY", "sk_test_51Qtr7NPx2zbqPK2lAZ1MZaeVkuIahLWGX6oBPq0LWe9EROuWSt8Ft8U6jNRKI0xCCxLMQE69OqOMk0iy4Y7O5vU100PN0bLWVq")
+os.environ.setdefault('STRIPE_WH_SECRET', 'whsec_c480b6668565f496f6c2b04dd903afdf30c50557021a84eb5a5407ba08722f7d')
